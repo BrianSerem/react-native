f78239231825c77b904ed352dbb1ee4cd68e3375
brianserem <brianseremiat@gmail.com>
2024-05-23 19:11:18 +0300

    added video fetching functionality

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,14 +1,63 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import * as Animatable from 'react-native-animatable'
+import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native'
+import { useState } from 'react'
+import { icons } from '../constants'
+
+const tempUrl = "https://media.licdn.com/dms/image/D4D12AQHCdAYNU9TRsg/article-cover_image-shrink_720_1280/0/1695983654178?e=1721865600&v=beta&t=NOyYc-XCTlM0TumSUx_KxX0JtPBAVXvlKIEwnVI8UnM"
+
+
+const zoomIn = {
+    0: {
+        scale: 0.9
+    },
+    1: {
+        scale: 1
+    }
+}
+
+const zoomOut = {
+    0: {
+        scale: 1
+    },
+    1: {
+        scale: 0.9
+    }
+}
+
+const TrendingItem = ({ activeItem, item }) => {
+    const [play, setPlay] = useState(false)
+
+    return (
+        <Animatable.View
+            className="mr-5"
+            animation={activeItem.$id === item.$id ? zoomIn : zoomOut}
+            duration={500}
+        >
+            {play ? (<Text className="text-white"> Playing!</Text>) : (
+                <TouchableOpacity
+                    className="relative justify-center items-center"
+                    activeOpacity={0.1}
+                    onPress={() => setPlay(true)}>
+                    <ImageBackground source={{ uri: tempUrl}}
+                    className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"/>
+                    <Image source={icons.play} className="w-12 h-12 absolute" resizeMode='contain' />
+
+                </TouchableOpacity>
+            )}
+        </Animatable.View>
+    )
+}
 
 const Trending = ({ posts }) => {
+    const [activeItem, setActiveItem] = useState(posts[0])
+
     return (
         <FlatList
             horizontal
             data={posts}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-                <Text className='text-3xl text-white'> {item.id} </Text>
+                <TrendingItem activeItem={activeItem} item={item} />
             )}
         />
     )
--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,10 +1,36 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { useState } from 'react'
+import { icons } from '../constants'
 
 const VideoCard = ({ video: { title, thumbnail, video, users: { username, avatar } } }) => {
+    const tempUrl = "https://media.licdn.com/dms/image/D4D12AQHCdAYNU9TRsg/article-cover_image-shrink_720_1280/0/1695983654178?e=1721865600&v=beta&t=NOyYc-XCTlM0TumSUx_KxX0JtPBAVXvlKIEwnVI8UnM"
+
+    const [play, setPlay] = useState(false)
+
     return (
         <View className="flex-col items-center px-4 mb-14">
-            <Text className="text-2xl text-white"> {title}</Text>
+            <View className="flex-row gap-3 items-start">
+                <View className="justify-center items-center flex-row flex-1">
+                    <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+                        <Image source={{ uri: avatar }} className="w-full h-full rounded-lg" resizeMode='cover' />
+                    </View>
+                    <View className="justify-center flex-1 ml-3 gap-y-1">
+                        <Text className="text-white font-semibold text-sm" numberOfLines={1}> {title} </Text>
+                        <Text className="text-xs font-pregular text-gray-100" numberOfLines={1}> {username} </Text>
+                    </View>
+                </View>
+                <View className="pt-2">
+                    <Image source={icons.menu} className="h-5 w-5" resizeMode="contain" />
+                </View>
+            </View>
+            {play ? (<Text> Playing </Text>) :
+                (<TouchableOpacity
+                    className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
+                    activeOpacity={0.7}
+                    onPress={() => setPlay(true)}>
+                    <Image source={{ uri: tempUrl }} className="w-full h-full rounded-xl mt-3" resizeMode="cover" />
+                    <Image source={icons.play} className="w-12 h-12 absolute" resizeMode='contain' />
+                </TouchableOpacity>)}
         </View>
     )
 }
