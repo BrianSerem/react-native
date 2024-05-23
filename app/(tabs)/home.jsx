02cec0cf9e612ec718a825f8cc632214c242a8e2
brianserem <brianseremiat@gmail.com>
2024-05-23 14:59:21 +0300

    added video fetching functionality

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -3,31 +3,29 @@ import { useState, useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
+import VideoCard from '../../components/VideoCard'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
 import { getAllPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 
 const Home = () => {
-  const { data: posts } =  useAppwrite(getAllPosts)
-
-  console.log(posts)
-
+  const { data: posts, refetch } =  useAppwrite(getAllPosts)
   const [refreshing, setRefreshing] = useState(false)
 
 
   const onRefresh = async () => {
     setRefreshing(true)
-
+    await refetch()
     setRefreshing(false)
   }
 
   return (
     <SafeAreaView className="bg-primary border-2 h-full">
       <FlatList
-        data={[{ 'id': 1 }, { 'id': 2 }, { 'id': 3 }]}
+        data={posts}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (<Text className='text-3xl text-white'> {item.id} </Text>)}
+        renderItem={({ item }) => (<VideoCard video={item}/>)}
         ListHeaderComponent={() => (
           <View className='my-6 px-4 space-y-6'>
             <View className='justify-between items-start flex-row mb-6'>
