dc33315ff77dd4a3194a17da71f9780a47637a22
brianserem <brianseremiat@gmail.com>
2024-05-24 19:38:12 +0300

    added trending videos section

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -2,6 +2,7 @@ import * as Animatable from 'react-native-animatable'
 import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native'
 import { useState } from 'react'
 import { icons } from '../constants'
+import { Video, ResizeMode } from 'expo-av'
 
 const tempUrl = "https://media.licdn.com/dms/image/D4D12AQHCdAYNU9TRsg/article-cover_image-shrink_720_1280/0/1695983654178?e=1721865600&v=beta&t=NOyYc-XCTlM0TumSUx_KxX0JtPBAVXvlKIEwnVI8UnM"
 
@@ -33,25 +34,40 @@ const TrendingItem = ({ activeItem, item }) => {
             animation={activeItem === item.$id ? zoomIn : zoomOut}
             duration={500}
         >
-            {play ? (<Text className="text-white"> Playing!</Text>) : (
-                <TouchableOpacity
-                    className="relative justify-center items-center"
-                    activeOpacity={0.1}
-                    onPress={() => setPlay(true)}>
-                    <ImageBackground source={{ uri: tempUrl}}
-                    className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"/>
-                    <Image source={icons.play} className="w-12 h-12 absolute" resizeMode='contain' />
+            {play ?
+                (
+                    <Video
+                        source={{ uri: item.video }}
+                        className="w-52 h-72 rounded-[33px] mt-3 bg-white/10"
+                        resizeMode={ResizeMode.CONTAIN}
+                        useNativeControls
+                        shouldPlay
+                        onPlaybackStatusUpdate={(status) => {
+                            if (status.didJustFinish) {
+                                setPlay(false);
+                            }
+                        }}
+                    />
+                )
+                : (
+                    <TouchableOpacity
+                        className="relative justify-center items-center"
+                        activeOpacity={0.1}
+                        onPress={() => setPlay(true)}>
+                        <ImageBackground source={{ uri: tempUrl }}
+                            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40" />
+                        <Image source={icons.play} className="w-12 h-12 absolute" resizeMode='contain' />
 
-                </TouchableOpacity>
-            )}
+                    </TouchableOpacity>
+                )}
         </Animatable.View>
     )
 }
 
 const Trending = ({ posts }) => {
     const [activeItem, setActiveItem] = useState(posts[0])
-    const viewableItemsChange =({ viewableItems }) => {
-        if(viewableItems.length > 0) {
+    const viewableItemsChanged = ({ viewableItems }) => {
+        if (viewableItems.length > 0) {
             setActiveItem(viewableItems[0].key)
         }
     }
@@ -64,8 +80,8 @@ const Trending = ({ posts }) => {
             renderItem={({ item }) => (
                 <TrendingItem activeItem={activeItem} item={item} />
             )}
-            onViewableItemsChanged ={viewableItemsChange}
-            viewabilityConfig ={{
+            onViewableItemsChanged={viewableItemsChanged}
+            viewabilityConfig={{
                 itemVisiblePercentThreshold: 70
             }}
             contentOffset={{ x: 170 }}
