f6793238ba0aeec66fadb8a1e6c0e74bec819a30
brianserem <brianseremiat@gmail.com>
2024-05-26 19:53:05 +0300

    added trending videos section

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,9 +1,12 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { useState } from 'react'
 import { icons } from '../constants'
+import { Video, ResizeMode } from 'expo-av'
 
 const VideoCard = ({ video: { title, thumbnail, video, users: { username, avatar } } }) => {
     const tempUrl = "https://media.licdn.com/dms/image/D4D12AQHCdAYNU9TRsg/article-cover_image-shrink_720_1280/0/1695983654178?e=1721865600&v=beta&t=NOyYc-XCTlM0TumSUx_KxX0JtPBAVXvlKIEwnVI8UnM"
+    const vidUrl = "https://videos.pexels.com/video-files/8087321/8087321-uhd_2160_3840_25fps.mp4"
+
 
     const [play, setPlay] = useState(false)
 
@@ -23,7 +26,20 @@ const VideoCard = ({ video: { title, thumbnail, video, users: { username, avatar
                     <Image source={icons.menu} className="h-5 w-5" resizeMode="contain" />
                 </View>
             </View>
-            {play ? (<Text> Playing </Text>) :
+            {play ? (
+                <Video
+                    source={{ uri: vidUrl }}
+                    className="w-full h-60 rounded-xl mt-3"
+                    resizeMode={ResizeMode.CONTAIN}
+                    useNativeControls
+                    shouldPlay
+                    onPlaybackStatusUpdate={(status) => {
+                        if (status.didJustFinish) {
+                            setPlay(false);
+                        }
+                    }}
+                />
+            ) :
                 (<TouchableOpacity
                     className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
                     activeOpacity={0.7}
