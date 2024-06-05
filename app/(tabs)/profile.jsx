a020b338e154830badf8845a866c0800e5d31131
brianserem <brianseremiat@gmail.com>
2024-06-05 15:16:59 +0300

    started working on profile section

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,19 +1,21 @@
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
-import { useEffect } from 'react'
+import { View, FlatList, TouchableOpacity, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import SearchInput from '../../components/SearchInput'
 import VideoCard from '../../components/VideoCard'
 import EmptyState from '../../components/EmptyState'
-import { getUserPosts, searchPosts } from '../../lib/appwrite'
+import { getUserPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
-import { useLocalSearchParams } from 'expo-router'
 import { useGlobalContext } from '../../context/GlobalProvider'
 import { icons } from '../../constants'
+import InfoBox from '../../components/InfoBox'
 
 const Profile = () => {
 
   const { user, setUser, setIsLoggedIn } = useGlobalContext()
   const { data: posts } = useAppwrite(() => getUserPosts(user.$id))
+
+  const logOut = async () => {
+    await log;
+  }
 
 
   return (
@@ -24,9 +26,37 @@ const Profile = () => {
         renderItem={({ item }) => (<VideoCard video={item} />)}
         ListHeaderComponent={() => (
           <View className='w-full justify-center items-center mt-6 mb-12 px-4'>
-            <TouchableOpacity>
-              <Image  source={icons.logout} />
+            <TouchableOpacity
+              className="items-end w-full mb-10"
+              onPress={logOut}
+            >
+              <Image source={icons.logout} resizeMode="contain" className="w-6 h-6" />
             </TouchableOpacity>
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode='cover'
+              />
+            </View>
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+            <View className="flex-row mt-5">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle='Posts'
+                containerStyles="mr-10"
+                titleStyles="text-lg"
+              />
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
+              />
+            </View>
           </View>
         )}
         ListEmptyComponent={() => (
