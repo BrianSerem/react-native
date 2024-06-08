8bb208d4a8b931a77c154944f961d8d85f8fecfb
brianserem <brianseremiat@gmail.com>
2024-06-08 23:16:01 +0300

    started working on create video section

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,15 +1,18 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native'
 import { icons } from '../../constants'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import FormField from '../../components/FormField'
 
 const Create = () => {
   return (
-    <SafeAreaView className="bg-primary border-2 h-full">
-      <Text className="text-3xl">create</Text>
-      <TouchableOpacity>
-        <Image source={icons.upload} />
-      </TouchableOpacity>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-psemibold">
+          Upload Video
+        </Text>
+        <FormField placeholder="Qwera"/>
+      </ScrollView>
     </SafeAreaView>
   )
 }
